d7b7f43028759bddb758b2df269c3453aedbda96
trvv <trhuvan@gmail.com>
2025-09-27 16:46:04 +0700

    fix import file name

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getFavourites, getUserBooking, updateFavourite } from '../controllers/UserController.js';
+import { getFavourites, getUserBooking, updateFavourite } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
